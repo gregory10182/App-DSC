00c4237d093ea8670b43244173427e32dd2edce0
gregory10182 <Gregory10183@gmail.com>
2023-04-21 12:51:53 -0400

    Calendar update button working again

--- src/components/Day.jsx
+++ src/components/Day.jsx
@@ -1,19 +1,36 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
-export default function Day({day, dayW, month, percentage}) {
+export default function Day({day, dayW, data, percentage, handleSubmit}) {
 
   const [minimized, setMinimized] = useState(true);
 
   const [minimizedDelayed, setMinimizedDelayed] = useState(true);
 
 
-  const hideWhenMinimized = { 
-    opacity: minimized ? '0' : '1',
-    transition: minimized ? 'opacity 0.5s ease' : 'opacity 0.5s ease 0.5s',
-    display: minimizedDelayed ? 'none' : ''
+  useEffect(() => {
+    setTimeout(
+      () => {
+        setMinimizedDelayed(minimized);
+      },
+      minimizedDelayed ? 550 : 1000
+    );
+  }, [minimized]);
+
+
+  const hideForm = { 
+    display: minimizedDelayed ? 'none' : '',
+    opacity: '0',
+    transition: 'opacity 1s ease',
   }
+
+  const showForm= {
+    display: '',
+    opacity: minimizedDelayed ? '0' : '1',
+    transition: minimizedDelayed ? 'opacity 1s ease 1s' : 'opacity 1s ease',  
+  }
+
   const minimize = {
-    height: minimized ? "20px" : "130px",
+    height: minimized ? "35px" : "140px",
     transition: minimized ? 'height 1s ease 0.5s' : 'height 1s ease'
   }
 
@@ -24,30 +41,43 @@ export default function Day({day, dayW, month, percentage}) {
 
 
   return (
-    <div style={minimize}>
-      <p className="DayOfTheWeek">{dayW}</p>
-      <p>
-        {(
-          ((day.Venta + (day.Bonificacion / 1.19) + day.Recargas) /
-            (month.DailyGoal * percentage)) *
-          100
-        )
-          .toFixed(2)
-          .toLocaleString()}
-        %
-      </p>
-      <button onClick={() => {
-        setMinimized(!minimized)
-        setTimeout(() => {
-          setMinimizedDelayed(!minimizedDelayed)
-        }, minimizedDelayed ? 0 : 1000)
-        }
-        }>
-        <img style={rotateButton} src="https://cdn-icons-png.flaticon.com/512/25/25243.png" alt="" />
+    <div className="Day" style={minimize}>
+      <div className="MainData">
+        <p className="DayOfTheWeek">{dayW}</p>
+        <p className="DailyPercentage">
+          {(
+            ((day.Venta + day.Bonificacion / 1.19 + day.Recargas) /
+              (data.DailyGoal * percentage)) *
+            100
+          )
+            .toFixed(2)
+            .toLocaleString()}
+          %
+        </p>
+
+
+        <p className="DailySelled">
+          {(day.Venta + day.Bonificacion / 1.19 + day.Recargas)
+            .toLocaleString('es', { style: 'currency', currency: 'CLP' })}
+        </p>
+      </div>
+
+      <button
+        onClick={() => {
+          setMinimized(!minimized);
+        }}
+      >
+        <img
+          style={rotateButton}
+          src="https://cdn-icons-png.flaticon.com/512/25/25243.png"
+          alt=""
+        />
       </button>
+
+
       <form
         className="Report"
-        style={hideWhenMinimized}
+        style={minimized ? hideForm : showForm}
         onSubmit={(e) => handleSubmit(e)}
       >
         <label htmlFor="SelledAtDay">Vta afecta: </label>
