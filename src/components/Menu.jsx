affd6a88595f4a949163357e3cd1c228ee975aff
Gregory Perez <Gregory10183@gmail.com>
2023-10-09 14:20:10 -0300

    Cambio menu dispositivos moviles

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -11,12 +11,10 @@ import styled from "styled-components";
 const StyledMenu = styled.div`
   background-color: #009635;
   display: flex;
-  justify-content: space-between;
+  justify-content: space-evenly;
   align-items: center;
-  width: 60%;
+  width: 100%;
   height: 2rem;
-  border-top-left-radius: 0.5rem;
-  border-top-right-radius: 0.5rem;
   padding: 0.6rem;
   padding-bottom: 0;
   position: fixed;
@@ -36,14 +34,11 @@ const StyledMenu = styled.div`
   }
 
   @media only screen and (min-width: 768px) {
-    width: 70%;
     height: 2.5rem;
   }
 
   @media only screen and (min-width: 1024px) {
     border-radius: 0;
-    border-top-right-radius: 0.5rem;
-    border-bottom-right-radius: 0.5rem;
     border: 0.15rem solid #e9a42b;
     border-top: none;
     border-bottom: none;
