1cb2db5850b73a2ab6a5d63713dedf2aa8fdc189
Gregory Perez <Gregory10183@gmail.com>
2023-10-16 17:05:29 -0300

    Haciendo el alert mas visible

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -1,24 +1,38 @@
 import React, { useEffect, useState } from "react";
 
-export default function Alert({Alert, Message}) {
+import styled from "styled-components";
 
-    const [cName, setCName] = useState("Alert")
+const StyledAlert = styled.div`
+  position: fixed;
+  top: 20px;
+  display: flex;
+  justify-content: center;
+  border: 0.2rem solid #e9a42b;
+  align-items: center;
+  opacity: 0;
+  width: auto;
+  padding: 0px 20px;
+  height: 30px;
+  border-radius: 10px;
+  background-color: #009635;
+  color: #ebebeb;
+  transition: all 0.5s ease;
+`;
 
-    useEffect(() => {
-        if(Alert === true & Message != ""){
-            setCName("AlertEntrance")
-        }else if(Alert === false & Message != ""){
-            setCName("AlertExit")
-        }else{
-            setCName("Alert")
-        }
-    }, [Alert])
+export default function Alert({ Alert, Message }) {
+  const appear = {
+    opacity: 1,
+  };
 
+  const disappear = {
+    opacity: 0,
+  };
 
-
-    return(
-        <div className={cName}>
-            <p>{Message}</p>
-        </div>
-    )
+  return (
+    <StyledAlert
+      style={(Alert === true) & (Message != "") ? appear : disappear}
+    >
+      <p>{Message}</p>
+    </StyledAlert>
+  );
 }
